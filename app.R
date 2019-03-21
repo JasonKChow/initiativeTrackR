44f24ccf95022a197a722fe0afff44e95f31a015
JasonKChow <jaso.chow@mail.utoronto.ca>
2019-03-21 13:41:38 -0400

    fist pass

--- app.R
+++ app.R
@@ -1,2 +1,62 @@
 library(shiny)
 library(plotly)
+
+if (file.exists('initiative.Rda')) {
+  load('initiative.Rda')
+} else {
+  initiative <- reactiveValues(df = data.frame(Character = character(),
+                                               Initiative = double(),
+                                               Order = integer()))
+}
+
+onStop(function() {
+  save(initiative, file = 'initiative.Rda')
+})
+
+#### UI ####
+ui <- fluidPage(
+  fluidRow(
+    column(6,
+           tableOutput('table')),
+    column(6,
+           textInput('charName', 'Name:', ''),
+           numericInput('initVal', 'Initiative', NaN),
+           actionButton('addChar', 'Add'))
+  ),
+
+  fluidRow(
+    column(1,
+           actionButton('next', 'Next')),
+    column(1,
+           actionButton('reset', 'Reset'))
+  )
+)
+
+#### Server ####
+server <- function(input, output, session) {
+  observeEvent(input$reset, {
+    initiative$df <<- data.frame(Character = character(),
+                                 Initiative = double(),
+                                 Order = integer())
+  })
+
+  observeEvent(input$addChar, {
+    if (input$charName != '') {
+      initiative$df <<- rbind(initiative$df,
+                              data.frame(Character = input$charName,
+                                         Initiative = input$initVal,
+                                         Order = -1))
+      updateTextInput(session, 'charName', value = '')
+      updateNumericInput(session, 'initVal', value = NaN)
+    }
+  })
+
+  output$table <- renderTable({
+    df <- initiative$df
+
+    df[sort(df$Initiative, index.return = TRUE, decreasing = TRUE)$ix,]
+  })
+}
+
+#### Show it! ####
+shinyApp(ui = ui, server = server)
